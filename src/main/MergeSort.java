85aa067f945633d55c76435b4bdd8b5a21d76dcf
liwei <861064676@qq.com>
2020-09-07 11:13:37 +0800

    可运行

--- src/main/MergeSort.java
+++ src/main/MergeSort.java
@@ -1,7 +1,9 @@
+package main;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+
 
 /**
  * Created by Administrator on 2018/4/8.
